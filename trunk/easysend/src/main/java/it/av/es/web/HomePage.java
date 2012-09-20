d4b4dbd4db0c1a0d5a82dc6ce1157dd780853312
a.vincelli@gmail.com <a.vincelli@gmail.com@6de8a768-92d9-1753-a867-ca14e03499b6>
2012-09-20 14:27:04 +0000

    primo form

--- trunk/easysend/src/main/java/it/av/es/web/HomePage.java
+++ trunk/easysend/src/main/java/it/av/es/web/HomePage.java
@@ -1,7 +1,6 @@
 package it.av.es.web;
 
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
-import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 
 /**
  * The page provides the home page.
@@ -14,62 +13,6 @@ public class HomePage extends BasePageSimple{
 
     public HomePage() {
         super();
-        add(new BookmarkablePageLink<String>("goUserManagerPage", UserManagerPage.class) {
-            @Override
-            protected void onBeforeRender() {
-                super.onBeforeRender();
-                setVisible((getApplication().getSecuritySettings().getAuthorizationStrategy()
-                        .isInstantiationAuthorized(UserManagerPage.class)));
-            }
-        });
-        add(new BookmarkablePageLink<String>("goProjectManagerPage", ProjectManagerPage.class) {
-            @Override
-            protected void onBeforeRender() {
-                super.onBeforeRender();
-                setVisible((getApplication().getSecuritySettings().getAuthorizationStrategy()
-                        .isInstantiationAuthorized(ProjectManagerPage.class)));
-            }
-        });
-        add(new BookmarkablePageLink<String>("goProductManagerPage", ProductManagerPage.class) {
-            @Override
-            protected void onBeforeRender() {
-                super.onBeforeRender();
-                setVisible((getApplication().getSecuritySettings().getAuthorizationStrategy()
-                        .isInstantiationAuthorized(ProductManagerPage.class)));
-            }
-        });
-        add(new BookmarkablePageLink<String>("goSelectProjectPage", SelectProjectPage.class) {
-            @Override
-            protected void onBeforeRender() {
-                super.onBeforeRender();
-                setVisible((getApplication().getSecuritySettings().getAuthorizationStrategy()
-                        .isInstantiationAuthorized(SelectProjectPage.class)));
-            }
-        });
-        add(new BookmarkablePageLink<String>("goPlaceNewOrderPage", PlaceNewOrderPage.class) {
-            @Override
-            protected void onBeforeRender() {
-                super.onBeforeRender();
-                setVisible((getApplication().getSecuritySettings().getAuthorizationStrategy()
-                        .isInstantiationAuthorized(PlaceNewOrderPage.class)));
-            }
-        });
-        add(new BookmarkablePageLink<String>("goCustomerManagerPage", CustomerManagerPage.class) {
-            @Override
-            protected void onBeforeRender() {
-                super.onBeforeRender();
-                setVisible((getApplication().getSecuritySettings().getAuthorizationStrategy()
-                        .isInstantiationAuthorized(CustomerManagerPage.class)));
-            }
-        });
-        add(new BookmarkablePageLink<String>("goSignOut", SignOut.class) {
-            @Override
-            protected void onBeforeRender() {
-                super.onBeforeRender();
-                setVisible((getApplication().getSecuritySettings().getAuthorizationStrategy()
-                        .isInstantiationAuthorized(SignOut.class)));
-            }
-        });
     }
     
 
